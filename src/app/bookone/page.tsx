948a9fceab5a0d4091b193b800919ad0aa431506
AlexTheWhiteHat <131209543+alex-aviles@users.noreply.github.com>
2023-11-17 14:55:01 -0700

    new page

---
+++ src/app/bookone/page.tsx
@@ -0,0 +1,11 @@
+export default async function bookOne() {
+    return (
+        <main>
+            <div>
+                <h1>
+                    Book One: Genesis of Gleipnir
+                </h1>
+            </div>
+        </main>
+    )
+}
