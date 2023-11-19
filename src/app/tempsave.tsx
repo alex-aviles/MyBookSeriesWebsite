8c6bf7d2ea00504466f75e398a9436de595e91ba
AlexTheWhiteHat <131209543+alex-aviles@users.noreply.github.com>
2023-11-18 17:47:16 -0700

    minor things

--- src/app/tempsave.tsx
+++ src/app/tempsave.tsx
@@ -94,3 +94,60 @@ export default function Home() {
 }
 
 */
+
+/* 11/18/23
+
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className="flex flex-col lg:flex-row min-h-screen p-5 lg:p-10">
+      
+      <div className="lg:w-1/4 lg:pr-5 lg:order-1">
+        <div className="lg:pt-2">
+          <Link href="/about" className="link">
+            <h2>About -&gt;</h2>
+            <p>Learn about me as the author of this series.</p>
+          </Link>
+        </div>
+        <div className="lg:pt-2">
+          <Link href="/bookone" className="link">
+            <h2>Book One -&gt;</h2>
+            <p>Genesis of Gleipnir&nbsp;</p>
+          </Link>
+        </div>
+        <div className="lg:pt-2">
+          <Link href="/booktwo" className="link">
+            <h2>Book Two -&gt;</h2>
+            <p>Apostasy In Aporlan</p>
+          </Link>
+        </div>
+        <div className="lg:pt-2">
+          <Link href="/bookthree" className="link">
+            <h2>Book Three -&gt;</h2>
+            <p>Maelstrom Upon Mieraud</p>
+          </Link>
+        </div>
+      </div>
+
+      <div className="lg:w-1/2 mb-8 lg:mb-0 lg:order-2">
+        <h1 className="text-4xl lg:text-9xl font-bold text-white">
+          Chronicles{' '}of{' '}Mieraud
+        </h1>
+      </div>
+
+      <div className="lg:w-1/4 lg:pl-5 lg:pr-2 lg:order-3">
+        <p className="flex justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-2 pt-3 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30">
+          <a 
+            href="https://www.amazon.com/dp/B09PHCFNH8?binding=paperback&qid=1700102595&sr=1-1&ref=dbs_dp_rwt_sb_pc_tpbk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Buy Now&nbsp;
+          </a>
+        </p>
+      </div>
+    </main>
+  );
+}
+*/
