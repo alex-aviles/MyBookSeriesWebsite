b88216c5fa4a253d9273ddd41902ea7e2d32e2e0
AlexTheWhiteHat <131209543+alex-aviles@users.noreply.github.com>
2023-11-22 20:48:12 -0800

    summary

--- src/app/bookone/page.tsx
+++ src/app/bookone/page.tsx
@@ -3,19 +3,28 @@ export default function BookOnePage() {
     <main className="hover:bg-blue-400/20 flex flex-row justify-around h-screen max-w-screen">
       
       <div className="hover:bg-blue-400/40 w-1/3 order-2">
-      <p className="text-[200%] font-bold text-center mt-[15%]">
+        <p className="text-[200%] font-bold text-center mt-[15%]">
           Book One:
         </p>
         <p className="text-[300%] font-bold text-center mt-[15%]">
           Genesis of Gleipnir
         </p>
+        <p className="text-lg max-w-prose text-center mt-[13%]">
+          ---Summary---
+        </p>
+        <p className="text-lg max-w-prose text-center mb-[20%]">
+        In the village of Canfis, Soren Gleipnir is a spurned boy 
+        who found work in a putrid but loving place. He goes on nightly walks 
+        around his hometown after work to ease his mind from the stresses his family's past placed on him. 
+        On one placid night, fumes fill the dark sky. The events of the night come to light and Soren is exiled from the village. 
+        With his childhood friend Rell, and a curious girl who knows him only by name, 
+        he sets off on a hunt for the royal Seal of Amnesty that will grant him acceptance back home.
+        </p>
       </div>
       <br></br>
       
       <div className="hover:bg-blue-400/10 w-1/3 order-1">
-        <p className="text-lg max-w-prose text-center">
-            Summary for Book One: Genesis of Gleipnir
-        </p>
+        
       </div>
 
       <div className="hover:bg-blue-400/40 w-1/3 order-3">
