81ed75a44995b9f497e69941654ba2ea7f12bd9b
AlexTheWhiteHat <131209543+alex-aviles@users.noreply.github.com>
2023-11-22 19:57:24 -0800

    understanding

--- src/app/bookone/page.tsx
+++ src/app/bookone/page.tsx
@@ -1,14 +1,25 @@
 export default function BookOnePage() {
   return (
-    <main className="hover:bg-blue-400/20 flex-row h-screen w-screen">
-      <div className="hover:bg-blue-400/40 order-3 text-4xl font-bold">
-        Genesis of Gleipnir
+    <main className="hover:bg-blue-400/20 flex flex-row justify-around h-screen max-w-screen">
+      
+      <div className="hover:bg-blue-400/40 w-1/3 order-2">
+      <p className="text-[200%] font-bold text-center mt-[15%]">
+          Book One:
+        </p>
+        <p className="text-[300%] font-bold text-center mt-[15%]">
+          Genesis of Gleipnir
+        </p>
       </div>
       <br></br>
-      <div className="hover:bg-blue-400/10 order-2">
-        <p className="text-lg max-w-prose">
+      
+      <div className="hover:bg-blue-400/10 w-1/3 order-1">
+        <p className="text-lg max-w-prose text-center">
             Summary for Book One: Genesis of Gleipnir
         </p>
+      </div>
+
+      <div className="hover:bg-blue-400/40 w-1/3 order-3">
+
       </div>
       
     </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex flex-row h-screen w-screen justify-around">
       
-      <div className="absolute top-[3%] sm:right-[%] md:top-[3%] md:right-[1%] lg:top-[3%] lg:right-[1%] xl:top-[3%] xl:right-[1%] items-center pr-[1%]">
+      <div className="absolute top-[3%] sm:right-[%] md:top-[3%] md:right-[1%] lg:top-[3%] lg:right-[1%] xl:top-[3%] xl:right-[1%] pr-[1%]">
         <p className="sm:p-2 md:p-3 lg:p-4 xl:p-5 dark:bg-zinc-800/30">
           <a 
             href="https://www.amazon.com/dp/B09PHCFNH8?binding=paperback&qid=1700102595&sr=1-1&ref=dbs_dp_rwt_sb_pc_tpbk"
@@ -17,7 +17,7 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="justify-center mt-[10%]">
+      <div className="mt-[10%]">
         <h1 className="font-bold text-white text-[200%] sm:text-[200%] md:text-[300%] lg:text-[500%] xl:text-[700%] leading-tight ">
           Chronicles{' '}<br></br>of{' '}Mieraud
         </h1>
