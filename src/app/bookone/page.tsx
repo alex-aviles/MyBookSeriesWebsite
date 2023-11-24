19db6a247966f1cf5a65fe0e2476cc373a5d3b87
AlexTheWhiteHat <131209543+alex-aviles@users.noreply.github.com>
2023-11-24 13:13:36 -0800

    BIG UPDATES for image

--- src/app/bookone/page.tsx
+++ src/app/bookone/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import bookOneCoverPage from '/public/bookonecover.png'
+import Image from "next/image"
+import bookonecover from '../bookone/bookonecover.png'
 
 export default function BookOnePage() {
 
@@ -21,19 +21,21 @@ export default function BookOnePage() {
           ---Summary---
         </p>
         <p className="text-lg max-w-prose text-center mb-[20%]">
-        In the village of Canfis, Soren Gleipnir is a spurned boy 
-        who found work in a putrid but loving place. He goes on nightly walks 
-        around his hometown after work to ease his mind from the stresses his family&apos;s past placed on him. 
-        On one placid night, fumes fill the dark sky. The events of the night come to light and Soren is exiled from the village. 
-        With his childhood friend Rell, and a curious girl who knows him only by name, 
-        he sets off on a hunt for the royal Seal of Amnesty that will grant him acceptance back home.
+          In the village of Canfis, Soren Gleipnir is a spurned boy 
+          who found work in a putrid but loving place. He goes on nightly walks 
+          around his hometown after work to ease his mind from the stresses his family&apos;s past placed on him. 
+          On one placid night, fumes fill the dark sky. The events of the night come to light and Soren is exiled from the village. 
+          With his childhood friend Rell, and a curious girl who knows him only by name, 
+          he sets off on a hunt for the royal Seal of Amnesty that will grant him acceptance back home.
         </p>
       </div>
 
       <div className="hover:bg-blue-400/40 w-1/3 order-3">
         <Image
-          src={bookOneCoverPage}
+          src={bookonecover}
           alt="Cover for Book One: Genesis of Gleinpir"
+          width={500}
+          height={500}
         />
       </div>
       
