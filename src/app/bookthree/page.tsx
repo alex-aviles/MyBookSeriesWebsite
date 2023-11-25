b6ed81cfd00833a7c259d1871f8343c124c36a0d
AlexTheWhiteHat <131209543+alex-aviles@users.noreply.github.com>
2023-11-24 20:15:10 -0800

    page templates

--- src/app/bookthree/page.tsx
+++ src/app/bookthree/page.tsx
@@ -1,7 +1,34 @@
-export default async function bookThree() {
-    return (
-        <main>
-            <h1>Book Three: Maelstrom Upon Mieraud</h1>
-        </main>
-    );
+import Image from "next/image"
+import bookOneCover from '../assets/bookonecover.png'
+
+export default function BookThreePage() {
+
+  return (
+    <main className="hover:bg-blue-400/20 flex flex-row justify-around h-screen max-w-screen">
+      
+      <div className="hover:bg-blue-400/10 w-1/3 order-1">
+        
+      </div>
+
+      <div className="hover:bg-blue-400/40 w-1/3 order-2">
+        <p className="text-[200%] font-bold text-center mt-[15%]">
+          Book Three:
+        </p>
+        <p className="text-[300%] font-bold text-center mt-[15%]">
+          Maelstrom Upon Mieraud
+        </p>
+        <p className="text-lg max-w-prose text-center mt-[13%]">
+          ---Summary---
+        </p>
+        <p className="text-lg max-w-prose text-center mb-[20%]">
+          {/* summary */}
+        </p>
+      </div>
+
+      <div className="hover:bg-blue-400/40 w-1/3 order-3 flex flex-col">
+        
+      </div>
+      
+    </main>
+  );
 }
--- src/app/booktwo/page.tsx
+++ src/app/booktwo/page.tsx
@@ -1,7 +1,34 @@
-export default async function bookTwoPage() {
-    return (
-        <main>
-            <h1>Book Two: Apostasy in Aporlan</h1>
-        </main>
-    );
+import Image from "next/image"
+import bookOneCover from '../assets/bookonecover.png'
+
+export default function BookTwoPage() {
+
+  return (
+    <main className="hover:bg-blue-400/20 flex flex-row justify-around h-screen max-w-screen">
+      
+      <div className="hover:bg-blue-400/10 w-1/3 order-1">
+        
+      </div>
+
+      <div className="hover:bg-blue-400/40 w-1/3 order-2">
+        <p className="text-[200%] font-bold text-center mt-[15%]">
+          Book Two:
+        </p>
+        <p className="text-[300%] font-bold text-center mt-[15%]">
+          Apostasy In Aporlan
+        </p>
+        <p className="text-lg max-w-prose text-center mt-[13%]">
+          ---Summary---
+        </p>
+        <p className="text-lg max-w-prose text-center mb-[20%]">
+          {/* summary */}
+        </p>
+      </div>
+
+      <div className="hover:bg-blue-400/40 w-1/3 order-3 flex flex-col">
+        
+      </div>
+      
+    </main>
+  );
 }
