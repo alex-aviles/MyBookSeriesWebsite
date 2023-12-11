f04ee727c519d6f27a1ffd3d9ae680fdd3710ecd
AlexTheWhiteHat <131209543+alex-aviles@users.noreply.github.com>
2023-12-10 20:16:29 -0800

    title updates

--- src/app/bookthree/page.tsx
+++ src/app/bookthree/page.tsx
@@ -12,7 +12,7 @@ export default function BookThreePage() {
 
       <div className="w-1/3 order-2">
         <p className="max-w-full text-[200%] font-bold text-center mt-[15%]">Book Three:</p>
-        <p className="max-w-full text-[300%] font-bold text-center mt-[15%]">Maelstrom Upon Mieraud</p>
+        <p className="max-w-full text-[300%] font-bold text-center mt-[15%]">Maelstrom upon Mieraud</p>
         <p className="max-w-full text-[150%] text-center mt-[13%]">---Summary---</p>
         <p className="max-w-full text-[150%] text-center mt-[5%]">coming soon</p>
       </div>
--- src/app/booktwo/page.tsx
+++ src/app/booktwo/page.tsx
@@ -12,7 +12,7 @@ export default function BookTwoPage() {
 
       <div className="w-1/3 order-2">
         <p className="max-w-full text-[200%] font-bold text-center mt-[15%]">Book Two:</p>
-        <p className="max-w-full text-[300%] font-bold text-center mt-[15%]">Apostasy In Aporlan</p>
+        <p className="max-w-full text-[300%] font-bold text-center mt-[15%]">Apostasy in Aporlan</p>
         <p className="max-w-full text-[150%] text-center mt-[13%]">---Summary---</p>
         <p className="max-w-full text-[150%] text-center mt-[5%]">work in progress</p>
       </div>
